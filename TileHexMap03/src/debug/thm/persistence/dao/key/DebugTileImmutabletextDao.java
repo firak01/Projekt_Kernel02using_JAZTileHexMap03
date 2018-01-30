f8737d38425f5a2771c9b1ea41cabb8161686ad1
firak01 <firak01@t-online.de>
2018-01-30 19:39:37 +0100

    TileImmutabletextDao.createEntryForThiskey(lngThiskey)
    Name des Spielsteins - FleetVariant => Immutabletext

---
+++ TileHexMap03/src/debug/thm/persistence/dao/key/DebugTileImmutabletextDao.java
@@ -0,0 +1,241 @@
+package debug.thm.persistence.dao.key;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
+
+import debug.thm.persistence.keytable.DebugKeyTable_Version_TileDefaulttextTHM;
+import debug.thm.persistence.keytable.DebugKeyTable_Version_TileImmutabletextTHM;
+import use.thm.persistence.dao.KeyDao;
+import use.thm.persistence.dao.TileDao;
+import use.thm.persistence.dao.TileDefaulttextDao;
+import use.thm.persistence.dao.TileImmutabletextDao;
+import use.thm.persistence.dao.TroopArmyDao;
+import use.thm.persistence.event.VetoFlag4ListenerZZZ;
+import use.thm.persistence.hibernate.HibernateContextProviderSingletonTHM;
+import use.thm.persistence.interfaces.enums.IEnumSetTextTHM;
+import use.thm.persistence.model.Immutabletext;
+import use.thm.persistence.model.Key;
+import use.thm.persistence.model.KeyImmutable;
+import use.thm.persistence.model.Tile;
+import use.thm.persistence.model.Defaulttext;
+import use.thm.persistence.model.TileDefaulttext;
+import use.thm.persistence.model.TileImmutabletext;
+import use.thm.persistence.model.TroopArmy;
+import use.thm.persistence.model.TroopFleetVariant;
+import use.thm.persistence.model.TileDefaulttext.EnumTileDefaulttext;
+import use.thm.persistence.model.TileImmutabletext.EnumTileImmutabletext;
+import use.thm.persistence.model.TroopFleetVariant.EnumTroopFleetVariant;
+import basic.persistence.util.HibernateUtil;
+import basic.zBasic.ExceptionZZZ;
+import basic.zBasic.ReflectCodeZZZ;
+import basic.zBasic.util.datatype.enums.EnumSetDefaulttextUtilZZZ;
+import basic.zBasic.util.datatype.enums.EnumSetInnerUtilZZZ;
+import basic.zBasic.util.datatype.enums.EnumSetInnerUtilZZZ.ThiskeyEnumMappingExceptionZZZ;
+import basic.zKernel.KernelZZZ;
+
+public class DebugTileImmutabletextDao {
+
+	public static void main(String[] args) {
+		
+		main:{
+			try{
+				KernelZZZ objKernel = new KernelZZZ(); //Merke: Die Service Klasse selbst kann wohl nicht das KernelObjekt extenden!
+				HibernateContextProviderSingletonTHM objContextHibernate = HibernateContextProviderSingletonTHM.getInstance(objKernel);					
+				objContextHibernate.getConfiguration().setProperty("hibernate.hbm2ddl.auto", "update");  //! Jetzt erst wird jede Tabelle über den Anwendungsstart hinaus gespeichert UND auch wiedergeholt.
+				
+				DebugTileImmutabletextDao objDebug = new DebugTileImmutabletextDao();	
+				objDebug.debugDeleteAll();
+				
+				objDebug.debugCreateEntryForThiskey();
+				
+				objDebug.debugSearchKey();	
+				
+				objDebug.debugFindAll();
+			} catch (ExceptionZZZ e) {
+				e.printStackTrace();
+			}
+			System.out.println(ReflectCodeZZZ.getPositionCurrent() + ": ENDE ##############");	
+		}//end main;
+		
+	}
+	public DebugTileImmutabletextDao(){		
+	}
+	
+	
+	public boolean debugCreateEntryForThiskey(){
+		boolean bReturn = false;
+		main:{
+			try {				
+				KernelZZZ objKernel = new KernelZZZ(); //Merke: Die Service Klasse selbst kann wohl nicht das KernelObjekt extenden!
+				HibernateContextProviderSingletonTHM objContextHibernate = HibernateContextProviderSingletonTHM.getInstance(objKernel);					
+						
+				//###################
+				//1. Speichere den Defaulttext		
+				//
+				//1.1. Vorbereitung: Hole die anderen Objekte..
+				//####################
+				int iIndex = 0;
+				EnumTileImmutabletext[] objaType = TileImmutabletext.EnumTileImmutabletext.values();
+
+				//String s = objaType[0].name(); //Prasenzstudium .... also entsprechend was als Eigenschaft vorgeschlagen wird von TileDefaulttextType.Praesenzstudium
+				//String s = objaType[0].toString(); //dito
+				//String s = objaType[0].description(); //gibt es nicht, das @description wohl nur etwas für Tool ist, welches diese Metasprachlichen Annotiations auswertet.
+				
+				//String s = objaType[iIndex].name();
+				System.out.println("debugCreateEntry für Index " + iIndex);
+				
+				//Es gibt bei Immutable keine Setter. Daher alles nur im Konstruktor übergeben.	
+			    Long lngThiskey = objaType[iIndex].getThiskey(); //Das darf nicht NULL sein, sonst Fehler. Über diesen Schlüssel wird der Wert dann gefunden.
+			   	
+			    bReturn= this.debugCreateEntryForThiskey(lngThiskey);
+			    
+			    
+			} catch (ExceptionZZZ e) {
+				e.printStackTrace();
+			}
+			System.out.println(ReflectCodeZZZ.getPositionCurrent() + ": ENDE ##############");			
+		}//end main:
+		return bReturn;											
+	}
+	
+	public boolean debugCreateEntryForThiskey(long lThiskey){
+		boolean bReturn = false;
+		main:{
+			try {				
+				KernelZZZ objKernel = new KernelZZZ(); //Merke: Die Service Klasse selbst kann wohl nicht das KernelObjekt extenden!
+				HibernateContextProviderSingletonTHM objContextHibernate = HibernateContextProviderSingletonTHM.getInstance(objKernel);					
+						
+				//###################
+				//1. Speichere den Defaulttext
+				TileImmutabletextDao daoKey = new TileImmutabletextDao(objContextHibernate);
+				bReturn = daoKey.createEntryForThiskey(lThiskey);
+												
+			} catch (ExceptionZZZ e) {				
+				e.printStackTrace();
+			}
+			System.out.println(ReflectCodeZZZ.getPositionCurrent() + ": ENDE ##############");			
+						
+		}//end main:
+		return bReturn;											
+	}
+	
+	
+	
+	
+	public boolean debugSearchKey(){
+		boolean bReturn = false;
+		main:{
+			try {				
+				KernelZZZ objKernel = new KernelZZZ(); //Merke: Die Service Klasse selbst kann wohl nicht das KernelObjekt extenden!
+				HibernateContextProviderSingletonTHM objContextHibernate = HibernateContextProviderSingletonTHM.getInstance(objKernel);					
+				//Darüber hat diese Methode nicht zu befinden... objContextHibernate.getConfiguration().setProperty("hibernate.hbm2ddl.auto", "update");  //! Jetzt erst wird jede Tabelle über den Anwendungsstart hinaus gespeichert UND auch wiedergeholt.
+				
+				TileImmutabletextDao daoKey = new TileImmutabletextDao(objContextHibernate);
+				String sKeytype = new String("");
+				Long lngThiskey = new Long(1);
+				
+				//TODO GOON 2017-06-14: Hier CellId-Schlüsselwerte vorbereiten und an die daoTroop.readByCellId(...) übergeben...
+//				sKeytype = "NIXVALUE";
+//				Key objKey = daoKey.searchKey(sKeytype, lngThiskey);
+//				if(objKey==null){
+//					System.out.println("1. Abfrage: Erwartetes Ergebnis. Kein Key mit dem KeyType '" + sKeytype + "' und dem Thiskey '" + lngThiskey.toString() + "' gefunden.");
+//				}else{
+//					System.out.println("1. Abfrage: UNERWARTETES ERGEBNIS. Key mit dem KeyType '" + sKeytype + "' und dem Thiskey '" + lngThiskey.toString() + "' gefunden.");
+//				}
+				
+				sKeytype = "IMMUTABLETILETEXT";
+				Immutabletext objKey02 = (Immutabletext) daoKey.searchKey(sKeytype, lngThiskey );
+				if(objKey02==null){
+					System.out.println("2. Abfrage: UNERWARTETES ERGEBNIS. Kein Key mit dem KeyType '" + sKeytype + "' und dem Thiskey '" + lngThiskey.toString() + "' gefunden.");
+				}else{
+					System.out.println("2. Abfrage: Erwartetes Ergebnis. Key mit dem KeyType '" + sKeytype + "' und dem Thiskey '" + lngThiskey.toString() + "' gefunden.");					
+				}			
+				
+				String sLongtext = objKey02.getLongtext();
+				System.out.println("Longtext = " + sLongtext);
+			} catch (ExceptionZZZ e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}//end main:
+		return bReturn;		
+	}
+	
+	public boolean debugFindAll(){
+		boolean bReturn = false;
+		main:{
+			try {				
+				KernelZZZ objKernel = new KernelZZZ(); //Merke: Die Service Klasse selbst kann wohl nicht das KernelObjekt extenden!
+				HibernateContextProviderSingletonTHM objContextHibernate = HibernateContextProviderSingletonTHM.getInstance(objKernel);					
+				//Darüber hat diese Methode nicht zu entscheiden... objContextHibernate.getConfiguration().setProperty("hibernate.hbm2ddl.auto", "update");  //! Jetzt erst wird jede Tabelle über den Anwendungsstart hinaus gespeichert UND auch wiedergeholt.
+				
+				TileImmutabletextDao daoKey = new TileImmutabletextDao(objContextHibernate);				
+				String sKeytype = new String("IMMUTABLETILETEXT");		
+				Long lngThiskey = new Long(1);
+
+				TileImmutabletext objKey02 = (TileImmutabletext) daoKey.searchKey(sKeytype, lngThiskey );
+				if(objKey02==null){
+					System.out.println("2. Abfrage: UNERWARTETES ERGEBNIS. Kein Key mit dem KeyType '" + sKeytype + "' und dem Thiskey '" + lngThiskey.toString() + "' gefunden.");
+				}else{
+					System.out.println("2. Abfrage: Erwartetes Ergebnis. Key mit dem KeyType '" + sKeytype + "' und dem Thiskey '" + lngThiskey.toString() + "' gefunden.");			
+					
+					//Nun alle holen
+					ArrayList<TileImmutabletext> listaTileDefaulttext = (ArrayList<TileImmutabletext>) daoKey.findLazyAll();
+					for(TileImmutabletext text : listaTileDefaulttext){
+						System.out.println("TileImmutbletext.toString(): " + text.toString());
+//						String sTYPE = "ARMY";
+//						String sValue = EnumSetDefaulttextUtilZZZ.readEnumConstant_DescriptionValue(text.getThiskeyEnumClass(), sTYPE);
+						Long lngThiskeyTemp = text.getThiskey();
+						String sDescriptionStored = text.getDescription();
+						System.out.println("Description (gespeichert): " + sDescriptionStored);
+						
+						String sDescriptionDefault = null; 
+						try {
+							String sType = EnumSetInnerUtilZZZ.getThiskeyEnum(text.getThiskeyEnumClass(), lngThiskeyTemp).name();							
+							System.out.println("Typ: " + sType);
+							
+							sDescriptionDefault = EnumSetDefaulttextUtilZZZ.readEnumConstant_DescriptionValue(text.getThiskeyEnumClass(), sType);
+							System.out.println("Description (Default): " + sDescriptionDefault);
+						} catch (ThiskeyEnumMappingExceptionZZZ e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						
+						if(sDescriptionStored.equals(sDescriptionDefault)){
+							System.out.println("Wert ist unverändert");							
+						}else{
+							System.out.println("WERT WURDE VERÄNDERT");
+						}												
+					}					
+				}		
+				
+			} catch (ExceptionZZZ e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}//end main:
+		return bReturn;		
+	}
+	
+	public boolean debugDeleteAll(){
+		boolean bReturn = false;
+		main:{
+			try {				
+				
+				KernelZZZ objKernel = new KernelZZZ(); //Merke: Die Service Klasse selbst kann wohl nicht das KernelObjekt extenden!
+				HibernateContextProviderSingletonTHM objContextHibernate = HibernateContextProviderSingletonTHM.getInstance(objKernel);					
+				//Darüber hat diese Methode nicht zu entscheiden objContextHibernate.getConfiguration().setProperty("hibernate.hbm2ddl.auto", "update");  //! Jetzt erst wird jede Tabelle über den Anwendungsstart hinaus gespeichert UND auch wiedergeholt.
+				
+				TileImmutabletextDao daoKey = new TileImmutabletextDao(objContextHibernate);
+				boolean bSuccess = daoKey.deleteAll();
+
+			} catch (ExceptionZZZ e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}//end main:
+		return bReturn;		
+	}
+}

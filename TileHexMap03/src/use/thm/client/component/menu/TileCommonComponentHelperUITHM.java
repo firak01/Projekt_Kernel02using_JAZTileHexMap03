c63546d93c449b6e62d32ad8b5b5ec250687511e
Firak01 <firak01@t-online.de>
2018-09-16 08:41:57 +0200

    Den Dialog zum Anzeigen der Detailangaben überarbeitet. Er wird nun sowohl per Doppelclick als auch aus dem Contextmenü heraus auch mit dem passenden GUI-Zoomlevel angezeigt.
    Signed-off-by: Firak01 <firak01@t-online.de>

--- TileHexMap03/src/use/thm/client/component/menu/TileCommonComponentHelperUITHM.java
+++ TileHexMap03/src/use/thm/client/component/menu/TileCommonComponentHelperUITHM.java
@@ -1,5 +1,6 @@
 package use.thm.client.component.menu;
 
+import java.awt.Font;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
@@ -7,10 +8,13 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
+import use.thm.ApplicationSingletonTHM;
 import use.thm.client.component.TileTHM;
 import use.thm.persistence.dto.ITileDtoAttribute;
+import use.zBasicUI.component.UIHelperTHM;
 import basic.persistence.dto.GenericDTO;
 import basic.zBasic.ExceptionZZZ;
 import basic.zBasic.util.datatype.string.StringZZZ;
@@ -22,93 +26,41 @@ public class TileCommonComponentHelperUITHM {
 		//TODO: Dies als eigene Dialogbox mit einem Customizbaren Panel, in dem z.B. eine Aktion "Auflösen" eingebaut wird.
 		//TODO GOON 20180324: Das wird immer aktueller, zumal jetzt aus dem DTO weitere Angaben aus der Datenbank geholte werden können.
 		//++++++++++++++++++++++++++++++++++++++++++++++++++++
-		String sMessage = new String();
+		String sMessage = _computeTileDetailString(objTile);
 		
-		//Hole weitere Informationen aus dem DTO:
-		GenericDTO<ITileDtoAttribute> objDto = objTile.getDto(); //this.getTile().getDto();
-		String sShorttext = objDto.get(ITileDtoAttribute.VARIANT_SHORTTEXT);
-		Integer intVariantUniquenumber = objDto.get(ITileDtoAttribute.INSTANCE_VARIANT_UNIQUENUMBER);
-		sMessage = sShorttext + "_" + intVariantUniquenumber.toString(); 
-				
-		//Anzeige der Koordinaten
+		//Leerzeile
+		sMessage = sMessage + StringZZZ.crlf();
+		
+		//Nur m Mouse Event steht der angelickte Punkt zur Verfügung
 		Point p = arg0.getPoint();
-		String sCoordinates = "X=" + objTile.getMapX() + "; Y=" + objTile.getMapY()  + " (" + p.getX() + "/" + p.getY() + ")";
-		sMessage = sMessage + StringZZZ.crlf() + sCoordinates;
-		//Trennzeile
-		sMessage = sMessage + StringZZZ.crlf();
-		Float fltHealth = objDto.get(ITileDtoAttribute.HEALTH);
-		float fHealthNormed = fltHealth.floatValue() * 100;
-		Float fltHealthNormed = new Float(fHealthNormed);
-		String sHealth = "Health: " + StringZZZ.left(fltHealthNormed.toString(),6)+"%";
-		sMessage = sMessage + StringZZZ.crlf() + sHealth;
-		
-		//Trennzeile
-		sMessage = sMessage + StringZZZ.crlf();
-					
-		String sUniquename = objDto.get(ITileDtoAttribute.UNIQUENAME);
-		sMessage = sMessage + StringZZZ.crlf() + sUniquename;
+		sMessage = sMessage + "Point clicked (X/Y) = (" + p.getX() + "/" + p.getY() + ")";
 		
 		//+++++++++++++++++++++++++++++
 		//Das ICON
-		//+++++++++++++++++++++++++++++
-		//TODO GOON 20180725: Das Bild passend zur Eingestellten Zoom Größe in der Applikation auswählen 
-		byte[] imageInByte = (byte[]) objDto.get(ITileDtoAttribute.VARIANT_IMAGEDIALOG_IN_BYTE_01); //Das unveränderte Bild aus der Datenbank, wird hier angezeigt.
-		BufferedImage objBufferedImage = UIHelper.toBufferedImage(imageInByte);
+		//+++++++++++++++++++++++++++++		 					
+		ImageIcon objImageIcon = _computeTileDetailImageIcon(objTile);
 		
-		//Erst jetzt ein größenverändetes ImageIcon aus dem BufferedImage machen. Merke: Ein Image oder ein BufferedImage funktioniert in der JOptionPane nicht
-		ImageIcon objImageIcon = new ImageIcon(objBufferedImage);
-		 					
 		//+++ AUSGABE +++++++++++++++++++++
-		JOptionPane.showMessageDialog(objTile, sMessage, "Detailangaben....", JOptionPane.INFORMATION_MESSAGE, objImageIcon);		
+		_showTileDetailDialog(objTile, sMessage, objImageIcon);	
 		//break;		
 	}
 	
 public static void showTileDetailDialog(TileTHM objTile, ActionEvent arg0) throws ExceptionZZZ{
-	//MERKE IN ACTION EVENT IST DIE ANZEIGE DER KOORDINATEN NICHT MÖGLICH	
+	//MERKE IN ACTION EVENT IST DIE ANZEIGE DER POINT-KOORDINATEN NICHT MÖGLICH	
 	try {
 	
 		//TODO: Dies als eigene Dialogbox mit einem Customizbaren Panel, in dem z.B. eine Aktion "Auflösen" eingebaut wird.
 		//TODO GOON 20180324: Das wird immer aktueller, zumal jetzt aus dem DTO weitere Angaben aus der Datenbank geholte werden können.
 		//++++++++++++++++++++++++++++++++++++++++++++++++++++
-		String sMessage = new String();
-		
-		//Hole weitere Informationen aus dem DTO:
-		GenericDTO<ITileDtoAttribute> objDto = objTile.getDto(); //this.getTile().getDto();
-		String sShorttext = objDto.get(ITileDtoAttribute.VARIANT_SHORTTEXT);
-		Integer intVariantUniquenumber = objDto.get(ITileDtoAttribute.INSTANCE_VARIANT_UNIQUENUMBER);
-		sMessage = sShorttext + "_" + intVariantUniquenumber.toString(); 
-				
-		//Anzeige der Koordinaten
-//		Point p = arg0.getPoint();
-//		String sCoordinates = "X=" + objTile.getMapX() + "; Y=" + objTile.getMapY()  + " (" + p.getX() + "/" + p.getY() + ")";
-//		sMessage = sMessage + StringZZZ.crlf() + sCoordinates;
-		//Trennzeile
-		sMessage = sMessage + StringZZZ.crlf();
-		Float fltHealth = objDto.get(ITileDtoAttribute.HEALTH);
-		float fHealthNormed = fltHealth.floatValue() * 100;
-		Float fltHealthNormed = new Float(fHealthNormed);
-		String sHealth = "Health: " + StringZZZ.left(fltHealthNormed.toString(),6)+"%";
-		sMessage = sMessage + StringZZZ.crlf() + sHealth;
-		
-		//Trennzeile
-		sMessage = sMessage + StringZZZ.crlf();
-					
-		String sUniquename = objDto.get(ITileDtoAttribute.UNIQUENAME);
-		sMessage = sMessage + StringZZZ.crlf() + sUniquename;
+		String sMessage = _computeTileDetailString(objTile); 
 		
 		//+++++++++++++++++++++++++++++
 		//Das ICON
 		//+++++++++++++++++++++++++++++
-		//TODO GOON 20180725: Das Bild passend zur Eingestellten Zoom Größe in der Applikation auswählen 
-		byte[] imageInByte = (byte[]) objDto.get(ITileDtoAttribute.VARIANT_IMAGEDIALOG_IN_BYTE_01); //Das unveränderte Bild aus der Datenbank, wird hier angezeigt.
-		BufferedImage objBufferedImage = UIHelper.toBufferedImage(imageInByte);
+		ImageIcon objImageIcon = _computeTileDetailImageIcon(objTile);	
 		
-		
-		//Erst jetzt ein größenverändetes ImageIcon aus dem BufferedImage machen. Merke: Ein Image oder ein BufferedImage funktioniert in der JOptionPane nicht
-		ImageIcon objImageIcon = new ImageIcon(objBufferedImage);
-		 					
 		//+++ AUSGABE +++++++++++++++++++++
-		JOptionPane.showMessageDialog(objTile, sMessage, "Detailangaben....", JOptionPane.INFORMATION_MESSAGE, objImageIcon);		
+		_showTileDetailDialog(objTile, sMessage, objImageIcon);		
 		//break;
 		
 	} catch (IOException e) {
@@ -116,6 +68,89 @@ public static void showTileDetailDialog(TileTHM objTile, ActionEvent arg0) throw
 		e.printStackTrace();
 	}
 	}
+
+
+/**Hole per Reflection aus der DTO-Attribut Klasse das Bild, welches zur Auflösung passt.
+	 * Hier: Übergebener HexMapZoomFactor-ALIAS.
+	 * 
+	 * @return
+	 * @throws ExceptionZZZ
+	 */
+	public static byte[] getVariantImageUsedInByte(TileTHM objTile, String sZoomFactorAlias) throws ExceptionZZZ{
+		GenericDTO<ITileDtoAttribute>objDto = objTile.getDto();
+		return UIHelperTHM.getVariantImageUsedInByte(objDto,"IMAGEDIALOG", sZoomFactorAlias);	
+	}
+
+private static ImageIcon _computeTileDetailImageIcon(TileTHM objTile) throws ExceptionZZZ, IOException {
+	ImageIcon objReturn = null;
+	main:{
+		//TODO GOON 20180725: Das Bild passend zur Eingestellten Zoom Größe in der Applikation auswählen 
+//				GenericDTO<ITileDtoAttribute> objDto = objTile.getDto();
+//				byte[] imageInByte = (byte[]) objDto.get(ITileDtoAttribute.VARIANT_IMAGEDIALOG_IN_BYTE_01); //Das unveränderte Bild aus der Datenbank, wird hier angezeigt.
+		
+				String sZoomFactorAlias = ApplicationSingletonTHM.getInstance().getGuiZoomFactorAliasCurrent();
+				byte[] imageInByte = TileCommonComponentHelperUITHM.getVariantImageUsedInByte(objTile, sZoomFactorAlias);
+				BufferedImage objBufferedImage = UIHelper.toBufferedImage(imageInByte);
+				
+				//Erst jetzt ein größenverändetes ImageIcon aus dem BufferedImage machen. Merke: Ein Image oder ein BufferedImage funktioniert in der JOptionPane nicht
+				objReturn = new ImageIcon(objBufferedImage);
+				 
+	}//end main
+	return objReturn;	
+}
+
+private static String _computeTileDetailString(TileTHM objTile) throws ExceptionZZZ {
+	String sReturn = new String("");
+	main:{
+		if(objTile==null) break main;
+				
+		//Hole weitere Informationen aus dem DTO:
+		GenericDTO<ITileDtoAttribute> objDto = objTile.getDto(); //this.getTile().getDto();
+		String sShorttext = objDto.get(ITileDtoAttribute.VARIANT_SHORTTEXT);
+		Integer intVariantUniquenumber = objDto.get(ITileDtoAttribute.INSTANCE_VARIANT_UNIQUENUMBER);
+		String sMessage = sShorttext + "_" + intVariantUniquenumber.toString(); 
+				
+		//Anzeige der Koordinaten
+		String sCoordinates = "X=" + objTile.getMapX() + "; Y=" + objTile.getMapY(); 
+		sMessage = sMessage + StringZZZ.crlf() + sCoordinates;
+		
+		//Trennzeile
+		sMessage = sMessage + StringZZZ.crlf();
+		
+		//Anzeige der Stärke
+		Float fltHealth = objDto.get(ITileDtoAttribute.HEALTH);
+		float fHealthNormed = fltHealth.floatValue() * 100;
+		Float fltHealthNormed = new Float(fHealthNormed);
+		String sHealth = "Health: " + StringZZZ.left(fltHealthNormed.toString(),6)+"%";
+		sMessage = sMessage + StringZZZ.crlf() + sHealth;
+		
+		//Trennzeile
+		sMessage = sMessage + StringZZZ.crlf();
+		
+		//Anzeige des technischen Namens
+		String sUniquename = objDto.get(ITileDtoAttribute.UNIQUENAME);
+		sMessage = sMessage + StringZZZ.crlf() + sUniquename;
+		
+		sReturn = sMessage;
+	}
+	return sReturn;
+}
+
+private static void _showTileDetailDialog(TileTHM objTile, String sMessage, ImageIcon objImageIcon) throws ExceptionZZZ{
+	
+	//Das wäre ohne die Angabe des Fonts JOptionPane.showMessageDialog(objTile, sMessage, "Detailangaben....", JOptionPane.INFORMATION_MESSAGE, objImageIcon);
+	
+	//Merke: Will man den Font in der JOptionPane ändern, muss man dies über ein JLabel Machen.
+	Font objFontGui = ApplicationSingletonTHM.getInstance().getGuiFontCurrent();
+	
+	//Merke: Will man Zeilnumbrüche in einem JLabel darstellen, so muss man dies mit HTML machen.
+	sMessage = StringZZZ.asHtml(sMessage);	
+		
+	JLabel label = new JLabel(sMessage);
+	label.setFont(objFontGui);
+	JOptionPane.showMessageDialog(objTile, label, "Detailangaben....", JOptionPane.INFORMATION_MESSAGE, objImageIcon);
+	
+}
 	
 	
 }

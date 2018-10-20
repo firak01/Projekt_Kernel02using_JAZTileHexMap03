78bc6721b93dd617e88bfffde0dce810c0327ccb
firak01 <firak01@t-online.de>
2018-10-20 13:52:57 +0200

    Systemkonfigurations-Defaults erweitert um SystemNumber.
    Signed-off-by: firak01 <firak01@t-online.de>

--- TileHexMap03/src/basic/zBasic/ConfigTHM.java
+++ TileHexMap03/src/basic/zBasic/ConfigTHM.java
@@ -2,10 +2,19 @@ package basic.zBasic;
 
 import basic.zKernel.KernelConfigZZZ;
 
+/**
+ *  Default Configuration of this project
+ * 
+ * @author lindhaueradmin, 20.10.2018, 06:35:47
+ * 
+ */
+@SuppressWarnings("unchecked")
 public class ConfigTHM extends KernelConfigZZZ{
+	private static final long serialVersionUID = 1L;
 	private static String sDIRECTORY_CONFIG_DEFAULT = "";
 	private static String sFILE_CONFIG_DEFAULT = "ZKernelConfigTileHexMap02Client.ini";
 	private static String sKEY_APPLICATION_DEFAULT = "THM";
+	private static String sNUMBER_SYSTEM_DEFAULT= "01";
 	
 	public ConfigTHM() throws ExceptionZZZ{
 		super();
@@ -31,6 +40,6 @@ public class ConfigTHM extends KernelConfigZZZ{
 	}
 	@Override
 	public String getSystemNumberDefault() {
-		return "01";
+		return ConfigTHM.sNUMBER_SYSTEM_DEFAULT;
 	}
 }
